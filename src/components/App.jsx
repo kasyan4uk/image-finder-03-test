0e8a467e4c858f99d56f01e36256b0f2cd10fba1
Serhii Kasianchuk <k.kasan2016@gmail.com>
2023-06-28 15:56:09 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import { Container } from "./App.styled";
+
+class App extends Component {
+    render() { 
+        return (
+            <Container>
+                
+            </Container>
+        );
+    }
+}
+ 
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+// eslint-disable-next-line no-lone-blocks
+{/* <header class="searchbar">
+  <form class="form">
+    <button type="submit" class="button">
+      <span class="button-label">Search</span>
+    </button>
+
+    <input
+      class="input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header> */}
